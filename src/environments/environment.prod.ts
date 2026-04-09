db7cbe5596917e7f76502f1d602b1cf1562ba16c
praveenlakanapuram <lpraveen004@gmail.com>
2026-04-09 15:53:04 +0530

    fix: Revoked & Regenerated the keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   sso: {
     domain: '206.189.130.216',
     authServiceUrl: 'https://206.189.130.216',
-    clientId: 'sso_687e6d5eef0308886eafd216e68f78a7',
+    clientId: 'sso_c9f4802b3f75f93ac8006200f1594b36',
     redirectUri: 'https://apsso-angular-demo-cnwf.vercel.app/auth/callback',
     scopes: 'openid profile email roles department',
   },
