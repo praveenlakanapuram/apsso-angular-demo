fe832171f4815e08e9112983c0a1ccc885635327
praveenlakanapuram <lpraveen004@gmail.com>
2026-04-09 16:01:50 +0530

    fix: New SSO ID

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   sso: {
     domain: '206.189.130.216',
     authServiceUrl: 'https://206.189.130.216',
-    clientId: 'sso_c9f4802b3f75f93ac8006200f1594b36',
+    clientId: 'sso_b9654fee6c60f1c035545832e5e7d10a',
     redirectUri: 'https://apsso-angular-demo-cnwf.vercel.app/auth/callback',
     scopes: 'openid profile email roles department',
   },
