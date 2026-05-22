21f5db159439b1f314042dfd031f7fcf8b01e917
praveenlakanapuram <lpraveen004@gmail.com>
2026-05-22 23:32:31 +0530

    Update environment Variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   sso: {
-    // domain: 'apsso.online',
-    // authServiceUrl: 'https://apsso.online',
-    // clientId: 'sso_b9654fee6c60f1c035545832e5e7d10a',
-    // redirectUri: 'https://apsso-angular-demo-cnwf.vercel.app/auth/callback',
-    domain: 'localhost:3007',
-    authServiceUrl: 'http://localhost:3007',
-    clientId: 'sso_28644cc46827981073e9db0d69a56049',
-    redirectUri: 'http://localhost:4200/auth/callback',
+    domain: 'apsso.online',
+    authServiceUrl: 'https://apsso.online',
+    clientId: 'sso_b9654fee6c60f1c035545832e5e7d10a',
+    redirectUri: 'https://apsso-angular-demo-cnwf.vercel.app/auth/callback',
+    // domain: 'localhost:3007',
+    // authServiceUrl: 'http://localhost:3007',
+    // clientId: 'sso_28644cc46827981073e9db0d69a56049',
+    // redirectUri: 'http://localhost:4200/auth/callback',
     scopes: 'openid profile email roles department',
   },
 };
